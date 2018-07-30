8ceb96d897955decf404987301b691e39ba2f898
Luis Garcia <garcia@yahoo.com>
2018-07-30 16:53:47 -0400

    User controller test

--- spec/controllers/users_controllers_spec.rb
+++ spec/controllers/users_controllers_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'    
 
 describe UsersController do
-  describe "POST create" do
-
+  describe "POST #create" do
     context "given a successful user creation" do
       let(:params) do
         {
@@ -43,10 +42,57 @@ describe UsersController do
         }
       end
 
-      it "" do 
+      it "creates an error message" do 
+        post(:create, params: {user: params})
+        expect(flash[:notice]).to be_present
+      end
 
+      it "renders the new template" do
+        post(:create, params: {user: params})
+        expect(response).to render_template(:new)
+      end
+    end
+  end
+
+  describe "GET #edit" do
+    context "modifying given user data" do
+      let(:params) do
+        { 
+          first_name: "lou", 
+          last_name: "gar",
+          email: "lou@gmail.com",
+          password: "pass",
+          password_confirmation: "pass"
+        }
+      end
+
+      it "responds to GET" do
+        post(:create, params: {user: params})
+        get(:edit, params: {id: User.last.id})
+        expect(response).to render_template(:edit)
+      end
+
+      it "responds to PATCH" do
+        post(:create, params: {user: params})
+        patch(:update, params: {id: 5, user: params})
+        expect(response).to be_redirect
       end
 
     end
   end
+
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
