610c7758f02ed003528b3e22f3e58332ec6de0d4
Luis Garcia <garcia50@yahoo.com>
2018-08-07 15:59:09 -0400

    Add content search logic in Org controller

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,7 +1,11 @@
 class OrganizationsController < ApplicationController
 
   def index
-    @organizations = Organization.paginate(page: params[:page])
+    if params[:search]
+      @organizations = Organization.search(params[:search]).paginate(page: params[:page])
+    else
+      @organizations = Organization.paginate(page: params[:page])
+    end
   end
 
   def show
