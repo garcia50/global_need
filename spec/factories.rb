4b8980faa5b36d251fa80d8cae196b4e6dec49f6
Luis Garcia <garcia50@yahoo.com>
2018-08-08 19:27:35 -0400

    Create factories for Org and User

---
+++ spec/factories.rb
@@ -0,0 +1,23 @@
+FactoryBot.define do 
+
+  factory :organization do 
+    sequence(:name) { |n| "Red Cross{n}"}
+    sequence(:description) { |n| "help the needy{n}"}
+    sequence(:bio) { |n| "people{n}"}
+    sequence(:email) { |n| "Red{n}@Cross.com"}
+    sequence(:phone) { |n| "{n}000000"}
+    sequence(:address) { |n| "{n}234 obt way"}
+    role 0
+  end
+
+  factory :user do
+    sequence(:first_name) { |n| "Mary#{n}"}
+    sequence(:last_name) { |n| "Davenport#{n}"}
+    sequence(:email) { |n| "maryDavenportNo#{n}@gmail.com"}
+    sequence(:phone) { |n| "{n}000000"}
+    sequence(:sex) { |n| "m{n}f"}
+    sequence(:skills) { |n| "walking{n}"}
+    sequence(:password) { |n| "password#{n}"}
+  end
+
+end
