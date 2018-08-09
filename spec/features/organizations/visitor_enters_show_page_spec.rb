13fc7b4c8df22890bdbb9c77cebd1f0bc2589e5f
Luis Garcia <garcia50@yahoo.com>
2018-08-09 19:58:17 -0400

    Add Org. show test

--- spec/features/organizations/visitor_enters_show_page_spec.rb
+++ spec/features/organizations/visitor_enters_show_page_spec.rb
@@ -4,10 +4,30 @@ describe "As a visitor, when I visit the organization index page" do
   describe "I click on an organization" do
     it "takes me to that organizations show page" do
       org = create(:organization)
+
+      visit root_path
+      click_on "Global Need"
+      expect(current_path).to eq organizations_path
+      
       visit organizations_path
       first('.panel > a').click
       expect(current_path).to eq organization_path(org)    
     end
+
+    it "should have organization content" do
+      org = create(:organization)
+
+      visit organizations_path
+      first('.panel > a').click
+
+      expect(page).to have_content('Red Cross3')
+      expect(page).to have_content('Who We Are')
+      expect(page).to have_content('people')
+      expect(page).to have_button('Sign In To Contact Us')
+    end
+
+    it ""
+
   end
 end
 
