0ecda169752c20834c6f944a9ae017709717db7e
Luis Garcia <garcia50@yahoo.com>
2018-08-09 15:38:33 -0400

    Create welcome spec test

---
+++ spec/features/welcome/user_visits_welcome_page_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'    
+
+describe "As a visitor" do
+  describe "I visit the welcome page " do
+    it "I see title and mission statement" do
+      visit root_path
+      expect(page).to have_content('Global Need')
+      expect(page).to have_content('Connect')
+      expect(page).to have_content('Grow')
+      expect(page).to_not have_content('Sleep')
+      expect(page).to have_link('login')
+      expect(page).to have_link('signup')
+    end
+
+    it "A visitor clicks on title link - enters Org. index" do 
+      visit root_path
+      click_on "Global Need"
+      expect(current_path).to eq organizations_path
+    end
+  end
+end
