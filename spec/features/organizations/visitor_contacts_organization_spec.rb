f52f423256752b2481a6ef35b78b01786bd2b8fd
Luis Garcia <garcia50@yahoo.com>
2018-08-13 21:19:09 -0400

    Modify Org. redirect test

--- spec/features/organizations/visitor_contacts_organization_spec.rb
+++ spec/features/organizations/visitor_contacts_organization_spec.rb
@@ -1,8 +1,17 @@
 require 'rails_helper'    
 
 describe "As an unauthorized visitor" do
-  describe "when I click on Sign In To Contact Us" do
-    it "redirects to login page" do
+  describe "after selecting a organization" do 
+    describe "when I click on Sign In To Contact Us" do
+      it "redirects to login page" do
+        org = create(:organization)
+
+        visit organization_path(org)
+
+        click_on "Sign In To Contact Us"
+
+        expect(current_path).to eq login_path
+      end
       org = create(:organization)
 
       visit organization_path(org)
