17897f026145125bfb7d3dad5c90add16ff23083
Luis Garcia <garcia50@yahoo.com>
2018-08-14 18:09:53 -0400

    Modify search case-insensitive

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,6 +2,6 @@ class Organization < ApplicationRecord
   validates_presence_of :name
 
   def self.search(search)
-    where("name LIKE ?", "%#{search}%") 
+    where("name iLIKE ?", "%#{search}%") 
   end
 end
