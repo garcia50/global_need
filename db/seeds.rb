e5af6efe4c894fb97a713fe9c7368f2416edeace
Luis Garcia <garcia50@yahoo.com>
2018-08-15 18:19:24 -0400

    Modify and add seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+OrgLocs.destroy_all
+Organization.destroy_all
+Location.destroy_all
+User.destroy_all
 
 100.times do 
   Organization.create(
@@ -26,3 +30,46 @@ end
     city: Faker::Address.city
   )
 end
+
+
+
+org = []
+loc = []
+
+100.times do 
+  org << Organization.create(
+    name: Faker::Company.name,
+    email: Faker::Internet.email, 
+    phone: Faker::PhoneNumber.cell_phone, 
+    description: Faker::Lorem.sentence,
+    bio: Faker::Lorem.paragraph(4),
+    address: Faker::Address.full_address
+  )
+end
+
+100.times do 
+  loc << Location.create(
+    long: Faker::Address.longitude.round(4),
+    lat: Faker::Address.latitude.round(4),
+    country: Faker::Address.country, 
+    state: Faker::Address.state, 
+    city: Faker::Address.city
+  )
+end
+
+org.each do |o|
+  loc.each do |l|
+    OrgLocs.create(
+      organization_id: o.id,
+      location_id: l.id
+    )
+  end
+end
+
+
+
+
+
+
+
+
