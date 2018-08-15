8f50a51c7d110d6f8232aca3f73e8b17527f8ac8
Luis Garcia <garcia50@yahoo.com>
2018-08-15 17:52:01 -0400

    Add proper association and validations

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,5 +1,10 @@
 class Organization < ApplicationRecord
   validates_presence_of :name
+  validates_uniqueness_of :name
+
+  # has_and_belongs_to_many :locations
+  has_many :org_locs
+  has_many :locations, through: :org_locs
 
   def self.search(search)
     where("name iLIKE ?", "%#{search}%") 
