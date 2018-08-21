857dafd3215042c5e11d918f701538798080357f
Luis Garcia <garcia50@yahoo.com>
2018-08-21 13:44:02 -0400

    Modify seeding to accommodate changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,33 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-OrgLocs.destroy_all
+LocationsOrganization.destroy_all
 Organization.destroy_all
 Location.destroy_all
 User.destroy_all
-
-100.times do 
-  Organization.create(
-    name: Faker::Company.name,
-    email: Faker::Internet.email, 
-    phone: Faker::PhoneNumber.cell_phone, 
-    description: Faker::Lorem.sentence,
-    bio: Faker::Lorem.paragraph(4),
-    address: Faker::Address.full_address
-  )
-end
-
-100.times do 
-  Location.create(
-    long: Faker::Address.longitude.round(4),
-    lat: Faker::Address.latitude.round(4),
-    country: Faker::Address.country, 
-    state: Faker::Address.state, 
-    city: Faker::Address.city
-  )
-end
-
-
 
 org = []
 loc = []
@@ -57,19 +34,9 @@ end
   )
 end
 
-org.each do |o|
-  loc.each do |l|
-    OrgLocs.create(
-      organization_id: o.id,
-      location_id: l.id
-    )
-  end
+org.each_with_index do |org, index|
+  LocationsOrganization.create(
+    organization_id: org.id,
+    location_id: loc[index].id
+  )
 end
-
-
-
-
-
-
-
-
