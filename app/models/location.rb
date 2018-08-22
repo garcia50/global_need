e54442e2e0dd8953965139f729d2196b2de99967
Luis Garcia <garcia50@yahoo.com>
2018-08-22 18:40:36 -0400

    Add geocode methods in Location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,14 @@
 class Location < ApplicationRecord
   has_many :locations_organizations
   has_many :organizations, through: :locations_organizations
+
+  geocoded_by :address
+  after_validation :geocode
+
+  reverse_geocoded_by :lat, :long
+  after_validation :reverse_geocode
+
+  def address
+    [country, state, city].compact.join(', ')
+  end
 end
