961626e3aa379c7f848475deaacb452c7cef2590
Luis Garcia <garcia50@yahoo.com>
2018-08-30 16:00:20 -0400

    Refactor and append jquery function

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,19 +1,18 @@
-var app = "<%= @locations.first.state.to_json %>"
-var app1 = "<%= @loca.to_json %>"
-var heyy = "<%= @hey%>"
-var loca = "<%= @loca %>"
+$(document).on("turbolinks:load", function() {
+  var map = document.getElementById('map')
+  var country = map.dataset.country
 
-handler = Gmaps.build('Google');
-handler.buildMap({ provider: {}, internal: {id: 'map'}}, function(){
-  var geocoder =  new google.maps.Geocoder();
-  geocoder.geocode( { 'address': loca}, function(results, status) {
-    r1 = results[0].geometry.location.lat();
-    r2 = results[0].geometry.location.lng();
+  handler = Gmaps.build('Google');
+  handler.buildMap({ provider: {}, internal: {id: 'map'}}, function(){
+    var geocoder =  new google.maps.Geocoder();
+    geocoder.geocode( { 'address': country }, function(results, status) {
+      r1 = results[0].geometry.location.lat();
+      r2 = results[0].geometry.location.lng();
 
-  // markers = handler.addMarkers(<%=raw @hey.to_json %>);
-    markers = handler.addMarkers([{"lat":r1,"lng":r2}]);
-    handler.bounds.extendWith(markers);
-    handler.fitMapToBounds();
-    handler.getMap().setZoom(5);
+      markers = handler.addMarkers([{"lat":r1,"lng":r2}]);
+      handler.bounds.extendWith(markers);
+      handler.fitMapToBounds();
+      handler.getMap().setZoom(5);
+    });
   });
 });
