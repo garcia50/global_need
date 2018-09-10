13a7f09803ed474334a64029201d45580bdd182e
Luis Garcia <garcia50@yahoo.com>
2018-09-10 13:41:55 -0400

    Change current account method name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,9 +21,7 @@ class UsersController < ApplicationController
     if @user.save
       session[:user_id] = @user.id
       flash[:notice] = "Welcome #{current_user.first_name}"
-      # path = params[:redirect_back] || user_path(@user)
-      path = params[:redirect_back].present? ? params[:redirect_back] : user_path(@account)
-
+      path = params[:redirect_back] || user_path(@user)
 
       redirect_to path
     else
@@ -33,7 +31,7 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = current_account
+    @user = current_user
   end
 
   def update
@@ -64,7 +62,7 @@ class UsersController < ApplicationController
   end
 
   def authorize_user
-    unless current_account.id == params[:id].to_i #In the future you can append additional statements here. ex: (current_user.id == params[user.id].to_i) && current_user.admin? 
+    unless current_user.id == params[:id].to_i #In the future you can append additional statements here. ex: (current_user.id == params[user.id].to_i) && current_user.admin? 
       redirect_to root_path
       flash[:notice] = "Opps!! You do not have access to this page."
     end
