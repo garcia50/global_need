a5ff5b411eb4d8a67d8a1b143aaf7c00fda73cb5
Luis Garcia <garcia50@yahoo.com>
2018-09-10 13:44:37 -0400

    Modify and restore application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_account
+  helper_method :current_user
 
-  def current_account
-    @current_account ||= if session[:account_type] == "User"
-      User.find(session[:account_id])
-    elsif session[:account_type] == "Organization"
-      Organization.find(session[:account_id])
-    end
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
   def check_user
-    redirect_to root_path if !current_account
+    redirect_to root_path if !current_user
   end
 end
