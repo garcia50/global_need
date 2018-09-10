e67d2ce865ba884811936fe626b0ffbbf7439fab
Luis Garcia <garcia50@yahoo.com>
2018-09-10 16:53:26 -0400

    Add user_id column to Org. table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_21_164226) do
+ActiveRecord::Schema.define(version: 2018_09_10_200635) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 2018_08_21_164226) do
     t.datetime "updated_at", null: false
     t.integer "role", default: 0
     t.string "address"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_organizations_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -56,4 +58,5 @@ ActiveRecord::Schema.define(version: 2018_08_21_164226) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "organizations", "users"
 end
