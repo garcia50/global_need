9ae5691f4d42c18907e048ccd81a1068992eb3bb
Luis Garcia <garcia50@yahoo.com>
2018-09-10 13:42:53 -0400

    remove secure password method and validations

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,9 +1,6 @@
 class Organization < ApplicationRecord
-  has_secure_password
-
-  validates_presence_of :name, :email, :password
+  validates_presence_of :name, :email
   validates_uniqueness_of :name
-  validates :password, confirmation: { case_senstive: true }
 
   has_many :locations_organizations
   has_many :locations, through: :locations_organizations
