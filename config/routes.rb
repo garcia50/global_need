81afc91ec4944c27ca21b39e53d7a5901003d1f4
Luis Garcia <garcia50@yahoo.com>
2018-09-13 20:50:28 -0400

    Add Org full actions

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "welcome#index"
 
-  resources :organizations, only: [:index, :show]
+  resources :organizations
   resources :users do
     collection do
       get 'org'
