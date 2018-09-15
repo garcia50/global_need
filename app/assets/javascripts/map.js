f9fdd11d630d4c0f95fe927480f146a1656d93b7
Luis Garcia <garcia50@yahoo.com>
2018-09-15 18:02:32 -0400

    Add geocode for Org office location

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -2,6 +2,7 @@ $(document).on("turbolinks:load", function() {
   var map = document.getElementById('map')
   var country = map.dataset.country
   var state = map.dataset.state
+  var address = map.dataset.offices
 
   handler = Gmaps.build('Google');
   handler.buildMap({ provider: {}, internal: {id: 'map'}}, function(){
@@ -18,6 +19,16 @@ $(document).on("turbolinks:load", function() {
 
 
     geocoder.geocode( { 'address': state }, function(results, status) {
+      r1 = results[0].geometry.location.lat();
+      r2 = results[0].geometry.location.lng();
+
+      markers = handler.addMarkers([{"lat":r1,"lng":r2}]);
+      handler.bounds.extendWith(markers);
+      handler.fitMapToBounds();
+      // handler.getMap().setZoom(5);
+    });
+
+    geocoder.geocode( { 'address': address }, function(results, status) {
       r1 = results[0].geometry.location.lat();
       r2 = results[0].geometry.location.lng();
 
