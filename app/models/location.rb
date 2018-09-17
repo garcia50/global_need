13978fa54b0343ddeb89ce7f7d45e21ad3cf2fd4
Luis Garcia <garcia50@yahoo.com>
2018-09-17 18:28:02 -0400

    Remove useless code

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,13 +2,16 @@ class Location < ApplicationRecord
   has_many :locations_organizations
   has_many :organizations, through: :locations_organizations
 
-  geocoded_by :address
-  after_validation :geocode
+  # geocoded_by :address
+  # after_validation :geocode
 
-  reverse_geocoded_by :lat, :long
-  after_validation :reverse_geocode
+  # reverse_geocoded_by :lat, :long
+  # after_validation :reverse_geocode
 
   def address
     [country, state, city].compact.join(', ')
   end
 end
+
+# org = Organization.find(121)
+#  loc = Location.create(country: "United States", state: "Georgia", city: "Los Angeles" )
