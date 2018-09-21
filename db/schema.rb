9450fe42d30f919ca2b057c700e6d0cd4a9b5c9c
Luis Garcia <garcia50@yahoo.com>
2018-09-21 15:44:35 -0400

    Add slug cloumn to users tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_10_200635) do
+ActiveRecord::Schema.define(version: 2018_09_20_194208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2018_09_10_200635) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
   end
 
   add_foreign_key "organizations", "users"
