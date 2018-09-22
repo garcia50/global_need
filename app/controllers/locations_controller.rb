7b18ec5d67ac4804ff8b558146269fc9e7eaf1f5
Luis Garcia <garcia50@yahoo.com>
2018-09-22 14:48:58 -0400

    Add edit and update actions, also add loacations params to successfully update a location

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -4,5 +4,31 @@ class LocationsController < ApplicationController
     @locations = @org.locations
   end
 
+  def edit
+    @loc = Location.find(params[:id])
+  end
+
+  def update
+    @org = current_user.organization
+    loc = Location.find(params[:id])
+    if loc.update(location_params)
+      flash[:notice] = "You've successfully updated locations for #{@org.name}"
+      redirect_to  organization_locations_path(@org)
+    else
+      flash.now[:error] = current_user.errors.full_messages 
+      render :edit
+    end
+  end
+
+  private
+
+  def location_params
+    params.require(:location).permit(:lat, 
+                                     :long, 
+                                     :country, 
+                                     :state, 
+                                     :city
+                                    )
+  end
 end
 
