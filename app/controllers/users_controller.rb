603db9acf7fc46a8fb77896585919342ee799532
Luis Garcia <garcia50@yahoo.com>
2018-09-24 18:57:10 -0400

    Refactor create org function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,15 +11,15 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    if params[:user][:organization].present?
-      @org = Organization.create(name: params[:user][:organization][:name], email: params[:user][:email], user: @user)
-    end
-
     if @user.save
       session[:user_id] = @user.id
       flash[:notice] = "Welcome #{current_user.first_name}"
       path = params[:redirect_back].present? ? params[:redirect_back] : user_path(@user)
       if params[:user][:organization].present?
+        @org = Organization.create(name: params[:user][:organization][:name],
+                                 email: params[:user][:organization][:email],
+                                 user: @user
+                                )
         path = edit_organization_path(@org)
       end
       redirect_to path
