c43f333a1e92578963c5f2cd360df279b49aa323
Luis Garcia <garcia50@yahoo.com>
2018-09-25 16:13:30 -0400

    Add org controllers spec

---
+++ spec/controllers/organizations_controllers_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'    
+
+describe OrganizationsController do
+  describe "POST #create" do
+    context "given a successful user creation" do
+          
+
+    end
+  end
+
+end
