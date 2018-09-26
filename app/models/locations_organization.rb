3afddaaf67a16767c4ecb0553395b029acc1f631
Luis Garcia <garcia50@yahoo.com>
2018-09-26 17:23:43 -0400

    Add Schema Info

--- app/models/locations_organization.rb
+++ app/models/locations_organization.rb
@@ -1,3 +1,11 @@
+# == Schema Information
+#
+# Table name: locationsorganization
+#
+# organization_id :bigint  
+# location_id     :bigint
+#
+
 class LocationsOrganization < ApplicationRecord
   belongs_to :organization
   belongs_to :location
