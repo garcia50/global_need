f7df4eadd16cbc97536ea3d7f58e420304c18adc
Luis Garcia <garcia50@yahoo.com>
2018-09-26 17:23:22 -0400

    Add Schema Info

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: organizations
+#
+# id         :bigint
+# name        :string  
+# description :string  
+# bio         :text    
+# email       :string  
+# phone       :string  
+# created_at  :datetime
+# updated_at  :datetime
+# address     :string  
+# user_id.    :bigint  
+#
+
 class Organization < ApplicationRecord
   validates_presence_of :name, :email, :address
   validates_uniqueness_of :name
