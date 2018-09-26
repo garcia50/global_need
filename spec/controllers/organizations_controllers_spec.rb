f6c57e579f53abe3733fd5d9820e29ef352ea4ea
Luis Garcia <garcia50@yahoo.com>
2018-09-26 17:22:25 -0400

    ready set the beginning of the org controller test

--- spec/controllers/organizations_controllers_spec.rb
+++ spec/controllers/organizations_controllers_spec.rb
@@ -2,8 +2,21 @@ require 'rails_helper'
 
 describe OrganizationsController do
   describe "POST #create" do
-    context "given a successful user creation" do
-          
+    context "given a successful org creation" do
+      let(:params) do
+        {
+          name: "lou", 
+          description: "gar",
+          bio: "Help build shelters throughout remote countries for desperate people.",
+          email: "lou@gmail.com",
+          phone: "800-1RESCUE"
+          address: "1000 W. Colonial Dr, Orlando, Florida, United States"
+          user_id: @user
+        }
+      
+      let(:user) { User.create(params) }
+
+      end    
 
     end
   end
