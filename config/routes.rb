31674a52bad03cbe725a8fe6ba32d91dfd88405c
Luis Garcia <garcia50@yahoo.com>
2018-09-27 15:05:18 -0400

    Remove slug feature on users

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "welcome#index"
 
-  resources :users, param: :slug do
+  resources :users do
     collection do
       get 'org'
     end
