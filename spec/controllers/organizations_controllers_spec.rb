4882a369a2fdc2a14126a4ffc3a13ddc9a5052eb
Luis Garcia <garcia50@yahoo.com>
2018-09-27 15:11:53 -0400

    Temp. comment out test

--- spec/controllers/organizations_controllers_spec.rb
+++ spec/controllers/organizations_controllers_spec.rb
@@ -3,20 +3,24 @@ require 'rails_helper'
 describe OrganizationsController do
   describe "POST #create" do
     context "given a successful org creation" do
-      let(:params) do
-        {
-          name: "lou", 
-          description: "gar",
-          bio: "Help build shelters throughout remote countries for desperate people.",
-          email: "lou@gmail.com",
-          phone: "800-1RESCUE"
-          address: "1000 W. Colonial Dr, Orlando, Florida, United States"
-          user_id: @user
-        }
+      # let(:params) do
+      #   {
+      #     name: "lou", 
+      #     description: "gar",
+      #     bio: "Help build shelters throughout remote countries for desperate people.",
+      #     email: "lou@gmail.com",
+      #     phone: "800-1RESCUE",
+      #     address: "1000 W. Colonial Dr, Orlando, Florida, United States",
+      #     user_id: @user
+      #   }
+      # end    
       
-      let(:user) { User.create(params) }
+      # let(:user) { User.create(params) }
 
-      end    
+      # it "redirects back to the user path" do
+      #   post(:create, params: {user: params})
+      #   expect(response).to redirect_to user_path(User.last.id)
+      # end
 
     end
   end
