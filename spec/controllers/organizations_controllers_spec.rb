d99fdf718c5d7ad3d463e108fa5d3f0294bcf31f
Luis Garcia <garcia50@yahoo.com>
2018-10-02 18:46:27 -0400

    Add org controller test- test success message

--- spec/controllers/organizations_controllers_spec.rb
+++ spec/controllers/organizations_controllers_spec.rb
@@ -21,8 +21,57 @@ describe OrganizationsController do
       #   post(:create, params: {user: params})
       #   expect(response).to redirect_to user_path(User.last.id)
       # end
+      
+      let(:user) { create(:user) }
+
+      # user = create(:user)
+      let(:params) do
+        {
+          name: "Health First",
+          email: "hf@health.org",
+          address: "601 E Rollins St, Orlando, FL 32803",
+          user_id: user.id
+        }
+      end
+
+      let(:org) { Organization.create(params) }
+
+      it "renders a success message" do
+require 'pry'; binding.pry
+        patch(:update, params: {id: org.id, user: params.merge(name: "FHC")} )
+        expect(flash[:notice]).to be_present
+      end
 
     end
   end
 
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
