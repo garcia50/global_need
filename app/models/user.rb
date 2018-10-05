5b48eb341975ea28742454aeb5199c89ab2f0ce2
Luis Garcia <garcia50@yahoo.com>
2018-10-04 20:48:00 -0400

    Fix spelling

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,11 +19,11 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates_presence_of :first_name, :last_name, :email, :password 
+  validates_presence_of :first_name, :last_name, :email, :password
   validates_uniqueness_of :email
   validates :password, confirmation: { case_senstive: true }
 
-  has_one :organzation
+  has_one :organization
   # before_save :generate_slug
 
   def generate_slug
