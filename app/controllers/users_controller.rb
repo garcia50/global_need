5ac32f90da988418e896647a231e5dcaa7378fd2
Luis Garcia <garcia50@yahoo.com>
2018-10-08 15:06:52 -0400

    Modify placement of authorize_user method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,6 +50,13 @@ class UsersController < ApplicationController
 
   private 
 
+  def authorize_user
+    unless current_user.id == params[:id].to_i #In the future you can append additional statements here. ex: (current_user.id == params[user.id].to_i) && current_user.admin? 
+      redirect_to root_path
+      flash[:error] = "Opps!! You do not have access to this page."
+    end
+  end
+
   def user_params
     params.require(:user).permit(
       :first_name, 
