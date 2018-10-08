c2c4cbf829273bae080b7e8b65898978139d335f
Luis Garcia <garcia50@yahoo.com>
2018-10-08 15:12:12 -0400

    Create update_user_org private method to allow users with organizations to update both their profile and their org profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -83,6 +83,20 @@ class UsersController < ApplicationController
     )
   end
 
+  def updated_user_org
+    @organization = current_user.organization
+    updated_user = current_user.update(user_params)
+    updated_org = @organization.update(org_params)
+    if updated_user && updated_org
+      flash[:notice] = "Your profile has been updated."
+      redirect_to user_path(current_user)
+    else
+      flash.now[:error] = current_user.errors.full_messages
+      flash.now[:error] += @organization.errors.full_messages
+      render :edit
+    end
+  end
+
     end
   end
 end
