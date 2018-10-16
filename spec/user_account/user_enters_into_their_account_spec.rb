225518cb52ebbc41e0c0f3924ebab8e713af2c53
Luis Garcia <garcia50@yahoo.com>
2018-10-16 17:34:50 -0400

    Create test- new user signup

--- spec/user_account/user_enters_into_their_account_spec.rb
+++ spec/user_account/user_enters_into_their_account_spec.rb
@@ -17,4 +17,23 @@ describe "As a user" do
       expect(page).to have_content("It's better to be a small part of something, than to be a big part of nothing.")    
       expect(user).to eq User.last
     end
+
+    it "I can signup as a new user" do 
+      visit '/'
+
+      click_on "signup"
+
+      fill_in('user_first_name', with: "John")
+      fill_in('user_last_name', with: "Doe")
+      fill_in('user_email', with: "john@doe.com")
+      fill_in('user_password', with: "pass")
+      fill_in('user_password_confirmation', with: "pass")
+
+      click_on "Submit"
+
+      expect(page).to have_content("Be the change you wish to see in the world!")
+      expect(page).to have_content("It's better to be a small part of something, than to be a big part of nothing.")    
+      expect(User.last.email).to eq "john@doe.com"
+    end
+  end
 end
